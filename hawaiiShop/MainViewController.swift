03945286923bff83af590a7773f984231e5274a2
steellson <a.steellson@gmail.com>
2022-11-23 15:26:13 +0300

    Initial

---
+++ hawaiiShop/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 23.11.2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupController()
+    }
+
+    private func setupController() {
+        view.backgroundColor = .systemGray
+    }
+
+}
+
