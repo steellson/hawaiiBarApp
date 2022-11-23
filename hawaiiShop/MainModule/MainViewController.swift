c770f5090b25f89c100efadc225dd3448863a096
steellson <a.steellson@gmail.com>
2022-11-23 22:51:11 +0300

    UI / Label Authorization added and based configured

--- hawaiiShop/MainModule/MainViewController.swift
+++ hawaiiShop/MainModule/MainViewController.swift
@@ -11,6 +11,16 @@ class MainViewController: UIViewController {
 
     var presenter: MainPresenterProtocol!
     
+    private let authorizationLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 28)
+        label.textAlignment = .center
+        label.contentMode = .center
+        label.numberOfLines = 0
+        label.text = "Authorization"
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,8 +31,22 @@ class MainViewController: UIViewController {
     
     private func setupController() {
         view.backgroundColor = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+        
+        view.addSubview(authorizationLabel)
+        authorizationLabelLayout()
     }
 
+//MARK: - Setup Layout
+    
+    private func authorizationLabelLayout() {
+        authorizationLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            authorizationLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            authorizationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
 }
 
 //MARK: Extension
