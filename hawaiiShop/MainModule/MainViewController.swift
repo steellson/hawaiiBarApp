74b347c2907ab9af662bbbe652307488915bca5d
steellson <a.steellson@gmail.com>
2022-11-25 14:06:18 +0300

    UI / UITextfield x2 / UIButton / Login field, password field and Login Button added and base configured

--- hawaiiShop/MainModule/MainViewController.swift
+++ hawaiiShop/MainModule/MainViewController.swift
@@ -31,13 +31,42 @@ class MainViewController: UIViewController {
         return view
     }()
     
-    private let authorizationModeSegment: UISegmentedControl = {
+    private lazy var authorizationModeSegment: UISegmentedControl = {
         let items = ["Sign Up", "Login"]
         let segment = UISegmentedControl(items: items)
         segment.setTitleTextAttributes([NSAttributedString.Key.font: UIFont(name: "Arial", size: 18)!], for: .normal)
         segment.backgroundColor = .white
         segment.isMomentary = false
         return segment
+    }()
+    
+    private lazy var loginField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "   Phone number"
+        field.layer.cornerRadius = 20
+        field.layer.borderWidth = 0.5
+        field.layer.borderColor = .init(red: 0, green: 0, blue: 0, alpha: 1)
+        field.contentMode = .scaleAspectFit
+        return field
+    }()
+    
+    private lazy var passwordField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "   SMS code"
+        field.layer.cornerRadius = 20
+        field.layer.borderWidth = 0.5
+        field.layer.borderColor = .init(red: 0, green: 0, blue: 0, alpha: 1)
+        field.contentMode = .left
+        return field
+    }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(red: 255/255, green: 82/255, blue: 9/255, alpha: 1)
+        button.setTitle("Log in", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.layer.cornerRadius = 16
+        return button
     }()
     
     override func viewDidLoad() {
@@ -59,6 +88,15 @@ class MainViewController: UIViewController {
         
         view.addSubview(authorizationModeSegment)
         authorizationModeSegmentLayout()
+        
+        view.addSubview(loginField)
+        loginFieldLayout()
+        
+        view.addSubview(passwordField)
+        passwordFieldLayout()
+        
+        view.addSubview(loginButton)
+        loginButtonLayout()
     }
     
     //MARK: - Setup Layout
@@ -91,6 +129,39 @@ class MainViewController: UIViewController {
             authorizationModeSegment.centerXAnchor.constraint(equalTo: authView.centerXAnchor),
             authorizationModeSegment.leftAnchor.constraint(equalTo: authView.leftAnchor, constant: 20),
             authorizationModeSegment.rightAnchor.constraint(equalTo: authView.rightAnchor, constant: -20)
+        ])
+    }
+    
+    private func loginFieldLayout() {
+        loginField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            loginField.topAnchor.constraint(equalTo: authorizationModeSegment.bottomAnchor, constant: 105),
+            loginField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginField.widthAnchor.constraint(equalToConstant: 340),
+            loginField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func passwordFieldLayout() {
+        passwordField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            passwordField.topAnchor.constraint(equalTo: loginField.bottomAnchor, constant: 25),
+            passwordField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            passwordField.widthAnchor.constraint(equalToConstant: 340),
+            passwordField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    private func loginButtonLayout() {
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            loginButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 120),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loginButton.widthAnchor.constraint(equalToConstant: 345),
+            loginButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
     
