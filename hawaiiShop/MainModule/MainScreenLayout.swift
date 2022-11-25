99ef27e7c1b739543bd51696a6271678c0d5903f
steellson <a.steellson@gmail.com>
2022-11-25 19:32:32 +0300

    Created Main module empty screen

---
+++ hawaiiShop/MainModule/MainScreenLayout.swift
@@ -0,0 +1,20 @@
+//
+//  MainScreenLayout.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 25.11.2022.
+//
+
+import Foundation
+import UIKit
+
+extension MainViewController {
+    
+    func setupLayout() {
+        
+    }
+    
+//MARK: - Methods
+    
+    
+}
---
+++ hawaiiShop/MainModule/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 25.11.2022.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupController()
+    }
+    
+    private func setupController() {
+        view.backgroundColor = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+
+        
+        setupLayout
+    }
+
+}
