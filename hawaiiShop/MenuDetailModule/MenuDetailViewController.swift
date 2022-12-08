4e2675bcb990817b3c45db062535caafd06b1339
steellson <a.steellson@gmail.com>
2022-12-08 22:07:41 +0300

    MenuDetailModule created ///////////////

---
+++ hawaiiShop/MenuDetailModule/MenuDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MenuDetailViewController.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 08.12.2022.
+//
+
+import Foundation
+import UIKit
+
+class MenuDetailViewController: UIViewController {
+    
+    //MARK: UI Elements
+    
+    
+    
+//MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupController()
+    }
+    
+    
+//MARK: - Setup Controller
+    
+    private func setupController() {
+        
+        
+        setupLayout()
+    }
+}
+
+
+//MARK: - Protocol Extension
+
+
+
+
---
+++ hawaiiShop/MenuDetailModule/MenuDetailViewControllerLayout.swift
@@ -0,0 +1,20 @@
+//
+//  MenuDetailLayout.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 08.12.2022.
+//
+
+import Foundation
+import UIKit
+
+extension MenuDetailViewController {
+    
+    func setupLayout() {
+        
+    }
+    
+//MARK: - Methods
+    
+    
+}
