f9d512527b71ad4df75baa5ff27345e9ae369a9b
steellson <a.steellson@gmail.com>
2022-12-09 15:16:01 +0300

    MenuDetailModule / MenuDetailCard Entity created

---
+++ hawaiiShop/MenuDetailModule/MenuDetailCard.swift
@@ -0,0 +1,19 @@
+//
+//  MenuDetailCard.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 09.12.2022.
+//
+
+import Foundation
+import UIKit
+
+struct MenuDetailCard {
+    
+    var titleLabel: String
+    var imageView: UIImageView?
+    var descriptionLabel: String
+    var weightLabel: String
+    var priceLabel: String
+    
+}
