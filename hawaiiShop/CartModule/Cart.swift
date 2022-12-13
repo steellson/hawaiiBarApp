1f2ee91923ab3f3a857d86236b00c1045b8e8766
steellson <a.steellson@gmail.com>
2022-12-13 15:14:37 +0300

    CartModule / Cart Models and mock data is created

---
+++ hawaiiShop/CartModule/Cart.swift
@@ -0,0 +1,27 @@
+//
+//  Cart.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 13.12.2022.
+//
+
+import Foundation
+import UIKit
+
+struct CartItem {
+    
+    var imageView: UIImageView?
+    var nameLabel: String
+    var priceLabel: String
+    
+}
+
+
+struct CartData {
+    
+    static var items: [CartItem] = [
+        CartItem(imageView: UIImageView(image: UIImage(named: "bowls")), nameLabel: "Chicken Breast Bowl", priceLabel: "16 $"),
+        CartItem(imageView: UIImageView(image: UIImage(named: "bowls")), nameLabel: "Tuna Bowl", priceLabel: "12 $"),
+        CartItem(imageView: UIImageView(image: UIImage(named: "pizza")), nameLabel: "Shrimps Pizza", priceLabel: "24 $")
+    ]
+}
