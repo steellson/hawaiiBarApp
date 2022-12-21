420c57e9333ee18d629575115db48d6d8b2a996b
steellson <a.steellson@gmail.com>
2022-12-21 21:13:33 +0300

    Created ProfileModule / initial configured

---
+++ hawaiiShop/ProfileModule/ProfileViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ProfileViewController.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 21.12.2022.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    //MARK: - UI ELements
+    
+    
+    
+    
+//MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupController()
+    }
+    
+    
+//MARK: - Setup Controller
+    
+    private func setupController() {
+        view.backgroundColor = UIColor(red: 244/255, green: 244/255, blue: 244/255, alpha: 1)
+
+        
+        setupNavigationBar()
+        setupLayout()
+    }
+    
+    private func setupNavigationBar() {
+        let leftBarItem = UIBarButtonItem(image: UIImage(systemName: "chevron.backward"), style: .plain, target: self, action: #selector(menuBarButtonDidTapped))
+        navigationItem.leftBarButtonItem = leftBarItem
+        navigationItem.leftBarButtonItem?.tintColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.6)
+        
+        navigationItem.title = "My profile"
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Quicksand-Bold", size: 20)!]
+    }
+    
+    
+    
+    //MARK: - Buttons Action
+    
+    @objc private func menuBarButtonDidTapped() {
+        print("Bar Button Did Tapped")
+    }
+    
+}
+
+
+//MARK: - Protocol Extension
+
+
+
+
+
---
+++ hawaiiShop/ProfileModule/ProfileViewControllerLayout.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileViewControllerLayout.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 21.12.2022.
+//
+
+import Foundation
+import UIKit
+
+
+extension ProfileViewController {
+    
+    
+    func setupLayout() {
+        
+        
+    }
+    
+    
+//MARK: - Methods
+    
+    
+}
