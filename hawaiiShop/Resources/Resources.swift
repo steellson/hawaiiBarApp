34a26658f10034270a38e36f2e883264e5b7bd9b
steellson <a.steellson@gmail.com>
2022-12-30 11:21:24 +0300

    Refactor / Resources fixed and added / Display error still not fixing

---
+++ hawaiiShop/Resources/Resources.swift
@@ -0,0 +1,28 @@
+//
+//  Resources.swift
+//  hawaiiShop
+//
+//  Created by Andrey Pochepaev on 30.12.2022.
+//
+
+import Foundation
+
+//MARK: - All app resources enum
+
+enum Resources {
+    
+    //MARK: - Cell identifiers
+    
+    enum Identifiers: String {
+        case mainMenuCardCell
+        case menuCardCell
+        case cartCell
+        case chooseMethodCell
+        case paymentMethodCell
+        case deliveryMethodCell
+        case orderCell
+        case ordersHistoryCell
+        case profileMenuCell
+    }
+    
+}
